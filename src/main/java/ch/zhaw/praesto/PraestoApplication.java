09295e42f6313f5cb24ad7d56e20a1aaa493439e
pericjul <pericjul@students.zhaw.ch>
2025-09-22 14:12:51 +0200

    first commit

---
+++ src/main/java/ch/zhaw/praesto/PraestoApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.praesto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraestoApplication.class, args);
+	}
+
+}
